f213d0945fe926f3a80d962bf90b1ba9eae50149
Purva Patel <purvapatel0198@gmail.com>
2022-11-28 17:56:59 -0500

    Added error handling for registration

--- Group_Project/group_project/registerapp/views.py
+++ Group_Project/group_project/registerapp/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
 from .forms import CreateUserForm
 
 
@@ -10,9 +9,12 @@ def registerView(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()   
-            username = form.cleaned_data.get('username')
-            email = form.cleaned_data.get('email')
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request,user)
             return redirect('login')
-
+    else:
+        form = CreateUserForm()
     context = {'form': form}
     return render(request, 'register.html', context)
