2bbbc6a6b07c172034cb9be1ebc6f53004ea6a8b
Purva Patel <purvapatel0198@gmail.com>
2022-12-01 17:18:31 -0500

    Error handling for login

--- Group_Project/group_project/loginapp/forms.py
+++ Group_Project/group_project/loginapp/forms.py
@@ -1,11 +1,11 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
 
 
-class UserRegistrationForm(UserCreationForm):
-    username = forms.CharField(max_length=50,required=True,widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
-    password = forms.CharField(max_length=50,required=True,widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(label=False,max_length=50,required=True,widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
+    password = forms.CharField(label=False,max_length=50,required=True,widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
 
     class Meta:
         model = User
