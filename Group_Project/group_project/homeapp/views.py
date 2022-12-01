e923c686ebde75c760084856cab86db8d8d97d69
“Jenibobo” <jenibobo04@gmail.com>
2022-12-01 16:49:31 -0500

    added a few comments to todo's view.py and homeapp's view.py file

--- Group_Project/group_project/homeapp/views.py
+++ Group_Project/group_project/homeapp/views.py
@@ -4,8 +4,9 @@ import requests
 
 
 def index(request):
-    today = str(datetime.date.today())
+    today = str(datetime.date.today()) # Getting todays date in iso format for the request to the api
 
+    # sending request to newsapi to get news feed data for climate news
     url = 'https://newsapi.org/v2/everything?q=Climate&from='+ today + '&sortBy=popularity&pageSize=20&apiKey=fbda378575da4f6bb90055f467a1ddab'
     news = requests.get(url).json()
 
@@ -15,6 +16,8 @@ def index(request):
     news_img = []
     news_url = []
 
+    # appending each piece of data to use to display the climate news feed for
+    # the home page.
     for i in range(len(a)):
         article = a[i]
         news_title.append(article['title'])
@@ -24,5 +27,3 @@ def index(request):
     mylist = zip(news_title, news_description, news_url, news_img)
     context = {'mylist': mylist}
     return render(request, 'base.html', context)
-
-    return render(request, 'base.html')
--- Group_Project/group_project/todoapp/views.py
+++ Group_Project/group_project/todoapp/views.py
@@ -1,5 +1,4 @@
-from pyexpat.errors import messages
-from django.http import HttpResponseRedirect
+
 from django.shortcuts import render, redirect
 
 
@@ -7,24 +6,31 @@ from . models import TodoListItem
 
 # Create your views here.
 
+# function used to render the todo html page and add the dict of to-dos
 def todoappView(request):
-    all_todo_items = TodoListItem.objects.all()
+    # gathers the todo obects from model.py
+    all_todo_items = TodoListItem.objects.all() 
+    # adds all todo items/objects into a dict for the html page to display
     todos = {
         "all_items": all_todo_items
     }
     return render(request, 'todo.html', todos)
 
 
+# function used to add a todo item/object to the users todo list
 def add_todoView(request):
-    x = request.POST['content']
-    if not x:
+    add_todo = request.POST['content']
+    if not add_todo:
         return redirect('todo')
     else:
-        new_item = TodoListItem(content = x)
+        new_item = TodoListItem(content = add_todo)
         new_item.save()
-        return redirect('todo')
+        return redirect('todo') # redirects the endpoint to the todo.html page
 
+
+# function that will aloow the user to delete a todo item/object from their 
+# todo list.
 def deleteTodoView(request, i):
-    y = TodoListItem.objects.get(id= i)
-    y.delete()
-    return redirect('todo')
+    delete_todo = TodoListItem.objects.get(id= i)
+    delete_todo.delete()
+    return redirect('todo') # redirects the endpoint to the todo.html page
